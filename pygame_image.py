f7086f0b0fde591c590f9f91b8575b155f8734b3
C0B22109 <c0b2210962@edu.teu.ac.jp>
2023-09-26 16:48:56 +0900

    背景画像が連続的になるように修正

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_rev = pg.transform.flip(bg_img, True, False)
+    bg_img_rev_flag = True
     charactor_img = pg.image.load("ex01/fig/3.png")
     charactor_img = pg.transform.flip(charactor_img, True, False)
 #    rotated_charactor_img = pg.transform.rotozoom(charactor_img, 10, 1.0)
@@ -21,9 +23,21 @@ def main():
             if event.type == pg.QUIT: return
 
         x = tmr % bgimg_width
-        screen.blit(bg_img, [-x, 0])
-        if x >= bgimg_width - display_width:
-            screen.blit(bg_img, [bgimg_width - x, 0])
+        if x == 0:
+            if bg_img_rev_flag:
+                bg_img_rev_flag = False
+            else:
+                bg_img_rev_flag = True
+
+        if bg_img_rev_flag:
+            screen.blit(bg_img_rev, [-x, 0])
+            if x >= bgimg_width - display_width:
+                screen.blit(bg_img, [bgimg_width - x, 0])
+        else:
+            screen.blit(bg_img, [-x, 0])
+            if x >= bgimg_width - display_width:
+                screen.blit(bg_img_rev, [bgimg_width - x, 0])
+
         img_list_index = tmr % (len(img_list) * 2)
         if img_list_index >= len(img_list):
             img_list_index = len(img_list) * 2 - 1 - img_list_index
